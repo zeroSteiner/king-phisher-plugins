9041665b5a3f2dc6269f4d6d852a6f03f884af37
Spencer McIntyre <zeroSteiner@gmail.com>
2019-05-19 14:01:51 -0400

    Update the save KPM On Exit plugin's message

--- client/kpm_export_on_exit.py
+++ client/kpm_export_on_exit.py
@@ -7,6 +7,7 @@ class Plugin(plugins.ClientPlugin):
 	title = 'Save KPM On Exit'
 	description = 'Prompt to save the message data as a KPM file when King Phisher exits.'
 	homepage = 'https://github.com/securestate/king-phisher-plugins'
+	version = '1.0.1'
 	def initialize(self):
 		if self.application.rpc is None:
 			self.signal_connect('server-connected', self.signal_server_connected)
@@ -15,7 +16,12 @@ class Plugin(plugins.ClientPlugin):
 		return True
 
 	def signal_exit_confirm(self, app):
-		if not gui_utilities.show_dialog_yes_no('Save KPM File?', app.get_active_window()):
+		response = gui_utilities.show_dialog_yes_no(
+			'Save KPM File?',
+			app.get_active_window(),
+			'Save the current message configuration to a KPM file?'
+		)
+		if not response:
 			return
 		mailer_tab = app.main_window.tabs['mailer']
 		mailer_tab.export_message_data()
